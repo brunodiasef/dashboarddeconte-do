8eb401ee6d899161ea7ab2fa79dbd42ca8ee3ab8
brunodiasef <brunopersonalsgp@gmail.com>
2026-09-08 09:09:05 -0300

    Update apresentacoes.js

--- apresentacoes.js
+++ apresentacoes.js
@@ -246,14 +246,14 @@ window.APRESENTACOES = [
     link: "aula/1ºtri/9ºano/caderno_educacao_fisica_9ano_1tri.pdf"
   },
   {
-    id: "aula-7t2n2-1788867505370",
-    ano: "7",
+    id: "aula-6t2n2-1788869326069",
+    ano: "6",
     trimestre: 2,
     aula: 2,
     titulo: "MATERIAL DE APOIO - manuscrito",
     descricao: "",
     tipo: "arquivo",
-    link: "aula/2ºtri/7ºano/APOSTILA.pdf"
+    link: "aula/2ºtri/6ºano/APOSTILA.pdf"
   }
   /* ADICIONE NOVAS AULAS ACIMA DESTA LINHA, separando por vírgula */
 ];
