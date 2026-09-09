42dc03c79b312b2c2aa6d492c75541373a7baa03
brunodiasef <brunopersonalsgp@gmail.com>
2026-09-09 15:20:38 -0300

    Update apresentacoes.js

--- apresentacoes.js
+++ apresentacoes.js
@@ -354,6 +354,16 @@ window.APRESENTACOES = [
     descricao: "",
     tipo: "arquivo",
     link: "aula/1ºtri/7ºano/jogo-mapa-mental-1-trimestre.html"
+  },
+  {
+    id: "aula-7t1n1-1788978016288",
+    ano: "7",
+    trimestre: 1,
+    aula: 1,
+    titulo: "MATERIAL DE APOIO - MANUSCRITO",
+    descricao: "",
+    tipo: "arquivo",
+    link: "aula/1ºtri/7ºano/caderno-7ano-1trimestre.pdf"
   }
   /* ADICIONE NOVAS AULAS ACIMA DESTA LINHA, separando por vírgula */
 ];
